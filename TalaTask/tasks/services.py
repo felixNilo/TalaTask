86631e648606af73f82b3dbec73d7694f176ad84
FelixNilo <felixjesusnilob@gmail.com>
2024-08-27 10:28:22 -0400

    Added readme.me and docker files.

--- TalaTask/tasks/services.py
+++ TalaTask/tasks/services.py
@@ -21,36 +21,35 @@ def assign_tasks():
             employee_skills = set(employee.skills.all())
             is_available_on_task_date = task_day_code in employee.available_days
             
-            #TODO. If is available, do the next, else, go to the other employee
-                
-            #check if the employee has the required skills
-            has_required_skills = required_skills.issubset(employee_skills)
+            if is_available_on_task_date:               
+                #check if the employee has the required skills
+                has_required_skills = required_skills.issubset(employee_skills)
 
-            #clculate the total available hours for the employee
-            total_available_hours = employee.available_hours_per_day * len(employee.available_days)
+                #clculate the total available hours for the employee
+                total_available_hours = employee.available_hours_per_day * len(employee.available_days)
 
-            #check if the employee has sufficient hours available
-            has_sufficient_hours = total_available_hours >= task.duration
+                #check if the employee has sufficient hours available
+                has_sufficient_hours = total_available_hours >= task.duration
 
-            if has_required_skills and is_available_on_task_date and has_sufficient_hours:
-                #assign the task to the employee
-                task.assigned_employee = employee
-                task.save()
+                if has_required_skills and is_available_on_task_date and has_sufficient_hours:
+                    #assign the task to the employee
+                    task.assigned_employee = employee
+                    task.save()
 
-                #reduce the employee available hours
-                total_available_hours -= task.duration
-                employee.available_hours_per_day = total_available_hours / len(employee.available_days)
-                employee.save()
-                
-                #update the assignments dictionary
-                assignments[employee.name]['total_hours_assigned'] += task.duration
-                assignments[employee.name]['remaining_hours'] = total_available_hours
-                assignments[employee.name]['tasks'].append({
-                    'task_title': task.title,
-                    'duration': task.duration,
-                    'skills_used': sorted([skill.name for skill in required_skills])
-                })
+                    #reduce the employee available hours
+                    total_available_hours -= task.duration
+                    employee.available_hours_per_day = total_available_hours / len(employee.available_days)
+                    employee.save()
+                    
+                    #update the assignments dictionary
+                    assignments[employee.name]['total_hours_assigned'] += task.duration
+                    assignments[employee.name]['remaining_hours'] = total_available_hours
+                    assignments[employee.name]['tasks'].append({
+                        'task_title': task.title,
+                        'duration': task.duration,
+                        'skills_used': sorted([skill.name for skill in required_skills])
+                    })
 
-                break  #task assigned, move to the next task
-    
+                    break  #task assigned, move to the next task
+        
     return assignments
